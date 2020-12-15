5afb6073d852dbf47feca089d2552278896d120d
sagifu <65776664+sagifu@users.noreply.github.com>
2020-12-15 12:21:29 +0200

    Add files via upload

--- Assignment 6/Sagi_Furman_Lesson6.m
+++ Assignment 6/Sagi_Furman_Lesson6.m
@@ -1,48 +1,68 @@
 %% Ques. 1
+% load picture
 puppiesImage = imread('puppies.jpg');
 %% Ques. 2
+% display picture without axis values
 imagesc(puppiesImage);
 axis off;
+title('original photo','FontSize',13);
 %% Ques. 3
-[whiteDogHead, WDPosition] = imcrop(puppiesImage);
-close;
-[blackDogHead, BDPosition] = imcrop(puppiesImage);
-close;
+% rectangle coordinates chosen using the image itself
+% rectangle coordinates [X Y Width Height]
+WDHeadPosition = [190 35 220 175];
+BDHeadPosition = [410 70 185 170];
+% using imcrop function, crop original picture according to the rectangle 
+% values
+whiteDogHead = imcrop(puppiesImage,WDHeadPosition);
+blackDogHead = imcrop(puppiesImage,BDHeadPosition);
 
+% plot
+% the axis of each picture will be with no values, and be displayed in a
+% square shape
 figure(); hold on;
 subplot(1,2,1);imagesc(whiteDogHead);
-axis off; axis square;
+axis off; axis square; title('white dog head','FontSize',13);
 subplot(1,2,2);imagesc(blackDogHead);
-axis off; axis square;
+axis off; axis square; title('black dog head','FontSize',13);
 %% Ques. 4
+% save head pictures using imwrite. inputs: the picture, filename
 imwrite(whiteDogHead, 'WDHead.jpg');
 imwrite(blackDogHead, 'BDHead.jpg');
 %% Ques. 5
 wDHead = fliplr(whiteDogHead);
 bDHead = fliplr(blackDogHead);
 %% Ques. 6
+figure();
 imagesc(puppiesImage); hold on;
-imagesc(WDPosition(1),WDPosition(2),wDHead);
-imagesc(BDPosition(1),BDPosition(2),bDHead);
+% using the X & Y values of the head position, we add the flipped head
+% pictures on the original picture' in the same location of the original
+% heads
+imagesc(WDHeadPosition(1),WDHeadPosition(2),wDHead);
+imagesc(BDHeadPosition(1),BDHeadPosition(2),bDHead);
+title('reverse heads','FontSize',13);
 %% Ques. 7
+% reduce pixle rate with self-written function
 mean5PuppiesImage = pixleReduction(puppiesImage,5);
 
 figure();
 subplot(1,2,1); imagesc(puppiesImage);
 axis off; axis square; title('original photo','FontSize',13);
 subplot(1,2,2); imagesc(mean5PuppiesImage);
-axis off; axis square; title('blurred photo','FontSize',13);
+axis off; axis square; title('blurred photo - 5 pixle reduction','FontSize',13);
 %% Ques. 8
+% reduce pixle rate with self-written function
 mean25PuppiesImage = pixleReduction(puppiesImage,25);
+% calculate the sum across 3rd dimension
+threeColorImage = sum(mean25PuppiesImage,3);
+% create three values image using boolean criteria
+% order is important! the first line MUST come before the other two
+threeColorImage(threeColorImage <= 600 & threeColorImage >= 150) = 155;
+threeColorImage(threeColorImage > 600) = 255;
+threeColorImage(threeColorImage < 150) = 0;
 
-threeColorImage = mean(mean25PuppiesImage,3);
-
-threeColorImage(threeColorImage > 200) = 255;
-threeColorImage(threeColorImage < 50) = 0;
-threeColorImage(threeColorImage <= 200 & threeColorImage >= 50) = 155;
-
-threeColorImage = uint8(threeColorImage);
-
+% plot
+figure();
 imagesc(threeColorImage);
+colormap gray
 axis off;
 title(['{\color{red}The ','\color{magenta}Image}'],'FontSize',14);
--- Assignment 6/pixleReduction.m
+++ Assignment 6/pixleReduction.m
@@ -1,34 +1,56 @@
 function image = pixleReduction(Image,pix2mean)
+% function pixleReduction recieves an image and a scalar, and reduces the
+% image clarity
+% INPUT: - Image - 3 dimensional array (RGB image)
+%        - pix2mean - a scalar indicating the square size of pixels that
+%        will be calculated into one pixel
+% OUTPUT: - image - input Image after reduction
 
+% extract number of rows and columns
 n_pix_row = size(Image,1);
 n_pix_col = size(Image,2);
 
+% calculate number of rows and columns in the new picture
+% if the original number of rows/columns doesn't divide in the number of
+% pixel reduction, round up using ceil
 n_new_col = ceil(n_pix_col/pix2mean);
 n_new_row = ceil(n_pix_row/pix2mean);
 
+% allocate output variable
 image = zeros(n_new_row,n_new_col,size(Image,3));
 
+% column counters
 col_strt = 1;
 col_end = pix2mean;
+% loop over NEW image column size
 for a = 1:n_new_col
+    % row counters reset
     row_strt = 1;
     row_end = pix2mean;
+    % loop over NEW image row size
     for b = 1:n_new_row
+        % calculate the mean of input Image pixels
         image(b,a,:) = mean(mean(Image(row_strt:row_end,col_strt:col_end,:)));
         
+        % advance counters
         row_strt = row_strt + pix2mean;
         row_end = row_end + pix2mean;
         
+        % make sure the last value isn't greater than the input Image
+        % dimnsions
         if row_end > n_pix_row
             row_end = n_pix_row;
         end
     end
+    % advance counters
     col_strt = col_strt + pix2mean;
     col_end = col_end + pix2mean;
     
+    % make sure the last value isn't greater than the input Image
+    % dimnsions
     if col_end > n_pix_col
         col_end = n_pix_col;
     end
 end
-
+% return output image in uint8 format
 image = uint8(image);
